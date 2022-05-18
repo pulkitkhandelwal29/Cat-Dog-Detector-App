cce1e6ccc0d612d77878d3c800e7c9b5122ba68b
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-05-19 03:19:56 +0530

    Creating home screen page

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,38 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Color(0x004242),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Coding Cafe',
+                style: TextStyle(
+                  color: Color(0x7D9E9E),
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Cats and Dogs Detector App',
+                style: TextStyle(
+                    color: Color(0x7D9E9E),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ));
   }
 }
