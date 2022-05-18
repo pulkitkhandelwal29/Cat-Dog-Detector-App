f8dd40326c6807fc0d5ce906a94c5abd1a50abb6
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-05-19 03:10:56 +0530

    Cat & Dog Detector App

---
+++ lib/splashscreen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import './home.dart';
+
+class MySplash extends StatefulWidget {
+  @override
+  State<MySplash> createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 3,
+      navigateAfterSeconds: Home(),
+      title: Text(
+        'Cat v/s Dog Detector',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 25,
+          color: Color(0x00FFFF),
+        ),
+      ),
+      image: Image.asset('assets/cat_dog_icon.png'),
+      backgroundColor: Colors.blueAccent,
+      photoSize: 60,
+      loaderColor: Color(0x004242),
+    );
+  }
+}
